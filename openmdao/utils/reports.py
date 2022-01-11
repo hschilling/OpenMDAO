d26d7f61ca6ca665fcad5999da8532349dc14dda
Herb Schilling <hschilling@nasa.gov>
2022-01-11 10:33:35 -0500

    Added code to list the reports

--- openmdao/utils/reports.py
+++ openmdao/utils/reports.py
@@ -21,8 +21,8 @@ def setup_default_reports():
     from openmdao.visualization.scaling_viewer.scaling_report import view_driver_scaling
     register_report(view_driver_scaling, 'view_driver_scaling', 'final_setup', 'post', 'driver', probname=None,
                     show_browser=False)
-    from openmdao.utils.coloring import coloring_reporting
-    register_report(coloring_reporting, 'coloring_reporting', 'final_setup', 'post', 'problem', probname=None)
+    # from openmdao.utils.coloring import coloring_reporting
+    # register_report(coloring_reporting, 'coloring_reporting', 'final_setup', 'post', 'problem', probname=None)
 
 
 # TODO Support env var of OPENMDAO_REPORTS with values of 0, false, off to disable all report generation
@@ -39,9 +39,75 @@ def set_reports_dir(reports_dir_path):
 
 
 # setup_default_reports()
+import sys
 
-def list_reports():
-    pass
+def list_reports(out_stream=None):
+    """
+    Write table of variable names, values, residuals, and metadata to out_stream.
+
+    Parameters
+    ----------
+    out_stream : file-like object
+        Where to send human readable output.
+        Set to None to suppress.
+    """
+    global reports_registry
+
+    if not out_stream:
+        out_stream = sys.stdout
+
+    column_names = ['probname', 'method', 'pre_or_post', 'desc', 'func']
+    column_widths = {}
+    # Determine the column widths of the data fields by finding the max width for all rows
+    for column_name in column_names:
+        column_widths[column_name] = len(column_name)
+
+    for report in reports_registry:
+        for column_name in column_names:
+            val = str(getattr(report, column_name))
+            column_widths[column_name] = max(column_widths[column_name], len(val))
+
+    out_stream.write("Here are the reports registered to run:\n\n")
+
+
+    # Write out the column headers
+    # column_header = '{:{align}{width}}'.format('varname', align=align,
+    #                                            width=max_varname_len)
+    column_header = ''
+    # column_dashes = max_varname_len * '-'
+    column_dashes = ''
+    column_spacing = 2
+    for i, column_name in enumerate(column_names):
+        column_header += '{:{width}}'.format(column_name, width=column_widths[column_name])
+        # column_header += column_name
+        column_dashes += column_widths[column_name] * '-'
+        if i < len(column_names) - 1:
+            column_header += column_spacing * ' '
+            column_dashes += column_spacing * ' '
+
+    out_stream.write('\n')
+    out_stream.write(column_header + '\n')
+    out_stream.write(column_dashes + '\n')
+
+    for report in reports_registry:
+        report_info = ''
+        for i, column_name in enumerate(column_names):
+            # report_info += '{:{width}}'.format(report[column_name], width=column_widths[column_name])
+            val = getattr(report, column_name)
+            if column_name == 'func':
+                val = val.__name__
+            else:
+                val = str(val)
+            val_formatted = f"{val:<{column_widths[column_name]}}"
+            report_info += val_formatted
+            # report_info += '{:{width}}'.format(val, width=column_widths[column_name])
+            if i < len(column_names) - 1:
+                report_info += column_spacing * ' '
+
+        # print(f"{report.probname} {report.method} {report.pre_or_post} {report.desc} {report.func}")
+        out_stream.write(report_info + '\n')
+
+    out_stream.write('\n')
 
 import os
 
@@ -76,5 +142,7 @@ def run_reports(prob, method, pre_or_post): # TODO can we use inspect to get met
                 reporting_object = prob
             elif report.reporting_object == 'driver':
                 reporting_object = prob.driver
+            else:
+                raise ValueError(f"Invalid reporting object {report.reporting_object}.")
             report.func(reporting_object, **report.kwargs)
             os.chdir(prev_cwd)
