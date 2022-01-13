b507cbd43ef62699ab7ff219117a294d0c67e38e
Herb Schilling <hschilling@nasa.gov>
2022-01-13 14:10:33 -0500

    Lots of code cleanup and PEP fixes

--- openmdao/utils/reports.py
+++ openmdao/utils/reports.py
@@ -2,47 +2,66 @@
 Utility functions related to the reporting system which generates reports by default for all runs.
 """
 
-import pathlib
-
-reports_registry = []
 from collections import namedtuple
+import pathlib
+import sys
+import os
+import contextlib
+from pathlib import Path
 
-Report = namedtuple('Report', 'probname reporting_object method pre_or_post func desc kwargs')
 
-def register_report(func, desc, method, pre_or_post, reporting_object, probname=None, **kwargs ):
-    global reports_registry
-    report = Report(probname, reporting_object, method, pre_or_post, func, desc, kwargs)
-    reports_registry.append(report)
+from openmdao.visualization.scaling_viewer.scaling_report import view_driver_scaling
+
+Report = namedtuple('Report', 'probname method pre_or_post func desc kwargs')
+
+_reports_registry = []
+_reports_dir = '.'  # the default location for the reports
+
+
+def register_report(func, desc, method, pre_or_post, probname=None, **kwargs):
+    global _reports_registry
+    report = Report(probname, method, pre_or_post, func, desc, kwargs)
+    _reports_registry.append(report)
     return
+
+
+def view_driver_scaling_for_report(prob, **kwargs):
+    """
+    Created for the reporting system, which expects the reporting functions to have Problem as
+    their first argument.
+
+    Parameters
+    ----------
+    prob : Problem
+        The problem used for the scaling report.
+    **kwargs : dict
+        Keyword args.
+
+    Returns
+    -------
+    dict
+        Data to used to generate html file.
+    """
+    return view_driver_scaling(prob.driver, **kwargs)
+
 
 def setup_default_reports():
     from openmdao.visualization.n2_viewer.n2_viewer import n2
-    register_report(n2, 'create n2', 'final_setup', 'post', 'problem', probname=None, show_browser=False)
-    from openmdao.visualization.scaling_viewer.scaling_report import view_driver_scaling
-    register_report(view_driver_scaling, 'view_driver_scaling', 'final_setup', 'post', 'driver', probname=None,
+    register_report(n2, 'create n2', 'final_setup', 'post', probname=None, show_browser=False)
+    register_report(view_driver_scaling_for_report, 'view_driver_scaling', 'final_setup', 'post',
+                    probname=None,
                     show_browser=False)
     from openmdao.utils.coloring import coloring_reporting
-    register_report(coloring_reporting, 'coloring_reporting', 'final_setup', 'post', 'problem', probname=None)
+    register_report(coloring_reporting, 'coloring_reporting', 'final_setup', 'post', probname=None)
 
-    pass
 
 setup_default_reports()
 
-# TODO Support env var of OPENMDAO_REPORTS with values of 0, false, off to disable all report generation
-# import inspect
-# import pathlib
-# script_path = inspect.stack()[-1][1]
-# script_name = pathlib.Path(script_path).stem
-# reports_dir = f'{script_name}_reports'
-reports_dir = '.'
 
 def set_reports_dir(reports_dir_path):
-    global reports_dir
-    reports_dir = reports_dir_path
+    global _reports_dir
+    _reports_dir = reports_dir_path
 
-
-# setup_default_reports()
-import sys
 
 def list_reports(out_stream=None):
     """
@@ -54,7 +73,7 @@ def list_reports(out_stream=None):
         Where to send human readable output.
         Set to None to suppress.
     """
-    global reports_registry
+    global _reports_registry
 
     if not out_stream:
         out_stream = sys.stdout
@@ -65,24 +84,18 @@ def list_reports(out_stream=None):
     for column_name in column_names:
         column_widths[column_name] = len(column_name)
 
-    for report in reports_registry:
+    for report in _reports_registry:
         for column_name in column_names:
             val = str(getattr(report, column_name))
             column_widths[column_name] = max(column_widths[column_name], len(val))
 
     out_stream.write("Here are the reports registered to run:\n\n")
 
-
-    # Write out the column headers
-    # column_header = '{:{align}{width}}'.format('varname', align=align,
-    #                                            width=max_varname_len)
     column_header = ''
-    # column_dashes = max_varname_len * '-'
     column_dashes = ''
     column_spacing = 2
     for i, column_name in enumerate(column_names):
         column_header += '{:{width}}'.format(column_name, width=column_widths[column_name])
-        # column_header += column_name
         column_dashes += column_widths[column_name] * '-'
         if i < len(column_names) - 1:
             column_header += column_spacing * ' '
@@ -92,10 +105,9 @@ def list_reports(out_stream=None):
     out_stream.write(column_header + '\n')
     out_stream.write(column_dashes + '\n')
 
-    for report in reports_registry:
+    for report in _reports_registry:
         report_info = ''
         for i, column_name in enumerate(column_names):
-            # report_info += '{:{width}}'.format(report[column_name], width=column_widths[column_name])
             val = getattr(report, column_name)
             if column_name == 'func':
                 val = val.__name__
@@ -103,62 +115,66 @@ def list_reports(out_stream=None):
                 val = str(val)
             val_formatted = f"{val:<{column_widths[column_name]}}"
             report_info += val_formatted
-            # report_info += '{:{width}}'.format(val, width=column_widths[column_name])
             if i < len(column_names) - 1:
                 report_info += column_spacing * ' '
 
-        # print(f"{report.probname} {report.method} {report.pre_or_post} {report.desc} {report.func}")
         out_stream.write(report_info + '\n')
 
     out_stream.write('\n')
 
-import os
 
-def run_reports(prob, method, pre_or_post): # TODO can we use inspect to get method?
-    global reports_dir
-    global reports_registry
+@contextlib.contextmanager
+def working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
+
+
+def run_reports(prob, method, pre_or_post):
+    global _reports_dir
+    global _reports_registry
+
+    # No running of reports when running under testflo
+    if 'TESTFLO_RUNNING' in os.environ:
+        return
 
     if 'OPENMDAO_REPORTS' in os.environ and os.environ['OPENMDAO_REPORTS'] in ['0', 'false', 'off']:
         return
 
-    reports_dir =  os.environ.get('OPENMDAO_REPORTS_DIR', reports_dir)
+    reports_dir = os.environ.get('OPENMDAO_REPORTS_DIR', _reports_dir)
 
     # need to get the report directory
     if not os.path.isdir(reports_dir):
         os.mkdir(reports_dir)
 
     # loop through reports registry looking for matches
-    for report in reports_registry:
+    for report in _reports_registry:
         if report.probname and report.probname != prob._name:
             continue
         if report.method != method:
             continue
         if report.pre_or_post == pre_or_post:
-            # TODO could use context mgr from here https://newbedev.com/how-can-i-change-directory-with-python-pathlib
             # make the problem reports dir
             problem_reports_dirname = f'{prob._name}_reports'
             problem_reports_dirpath = pathlib.Path(reports_dir).joinpath(problem_reports_dirname)
             if not os.path.isdir(problem_reports_dirpath):
                 os.mkdir(problem_reports_dirpath)
-            prev_cwd = pathlib.Path.cwd()
-            os.chdir(problem_reports_dirpath)
-            if report.reporting_object == 'problem':
-                reporting_object = prob
-            elif report.reporting_object == 'driver':
-                reporting_object = prob.driver
-            else:
-                raise ValueError(f"Invalid reporting object {report.reporting_object}.")
 
-            # report.func(reporting_object, **report.kwargs)
-            try:
-                report.func(reporting_object, **report.kwargs)
-            # Need to handle the coloring and scaling reports which can fail in this way
-            #   because total Jacobian can't be computed
-            except RuntimeError as err:
-                if str(err) != "Can't compute total derivatives unless both 'of' or 'wrt' variables have been specified.":
-                    raise err
-            os.chdir(prev_cwd)
+            with working_directory(problem_reports_dirpath):
+                try:
+                    report.func(prob, **report.kwargs)
+                # Need to handle the coloring and scaling reports which can fail in this way
+                #   because total Jacobian can't be computed
+                except RuntimeError as err:
+                    if str(err) != "Can't compute total derivatives unless " \
+                                   "both 'of' or 'wrt' variables have been specified.":
+                        raise err
+
 
 def clear_reports():
-    global reports_registry
-    reports_registry = []
+    global _reports_registry
+    _reports_registry = []
--- openmdao/utils/tests/test_reports_system.py
+++ openmdao/utils/tests/test_reports_system.py
@@ -2,29 +2,33 @@
 import unittest
 import pathlib
 import sys
+import os
 
 import openmdao.api as om
 from openmdao.test_suite.components.paraboloid import Paraboloid
+from openmdao.utils.reports import setup_default_reports, clear_reports, set_reports_dir, \
+    register_report, _reports_dir
+from openmdao.visualization.n2_viewer.n2_viewer import n2
 
 from openmdao.utils.testing_utils import use_tempdirs
 
-import openmdao
 
 @use_tempdirs
 class TestReportGeneration(unittest.TestCase):
 
     def setUp(self):
-        # from openmdao.utils.reports import setup_default_reports
+        import openmdao.core.problem
+
         self.n2_filename = 'n2.html'
         self.scaling_filename = 'driver_scaling_report.html'
         self.coloring_filename = 'jacobian_to_compute_coloring.png'
+
+        # set things to a known initial state for all the test runs
         openmdao.core.problem._problem_names = []  # need to reset these to simulate separate runs
 
-        import os
-        # del os.environ['OPENMDAO_REPORTS']
         os.environ.pop('OPENMDAO_REPORTS', None)
         os.environ.pop('OPENMDAO_REPORTS_DIR', None)
-        from openmdao.utils.reports import setup_default_reports, clear_reports, set_reports_dir
+        os.environ.pop('TESTFLO_RUNNING', None)
         clear_reports()
         set_reports_dir('.')
         setup_default_reports()
@@ -47,15 +51,8 @@ class TestReportGeneration(unittest.TestCase):
         prob.run_driver()
         prob.cleanup()
 
-        # get the report dir path
-        import inspect
-        script_path = inspect.stack()[-1][1]
-        script_name = pathlib.Path(script_path).stem
-        # reports_dir = f'{script_name}_reports'
-        reports_dir = '.'
-
         # get the path to the problem subdirectory
-        problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{prob._name}_reports')
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{prob._name}_reports')
 
         p = pathlib.Path(problem_reports_dir).joinpath(self.n2_filename)
         self.assertTrue(p.is_file(),f'The N2 report file, {str(p)} was not found')
@@ -118,16 +115,7 @@ class TestReportGeneration(unittest.TestCase):
         prob.cleanup()
 
         # See if the report files exist and if they have the right names
-        # script_name = pathlib.Path(sys.argv[-1]).stem
-        # reports_dir = f'{script_name}_reports'
-        reports_dir = '.'
-        # get the path to the problem subdirectory
-        problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{prob._name}_reports')
-
-        # n2_filename = f'{prob._name}_N2.html'
-        # scaling_filename = f'{prob._name}_driver_scaling.html'
-        # n2_filename = 'n2.html'
-        # scaling_filename = 'driver_scaling_report.html'
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{prob._name}_reports')
 
         p = pathlib.Path(problem_reports_dir).joinpath(self.n2_filename)
         self.assertFalse(p.is_file(),f'The N2 report file, {str(p)} was found but should not have')
@@ -172,15 +160,13 @@ class TestReportGeneration(unittest.TestCase):
     def test_report_generation_user_defined_report(self):
         from openmdao.utils.reports import register_report
 
-        import os
-
         user_report_filename = 'user_report.txt'
 
         def user_defined_report(prob):
             with open(user_report_filename, "w") as f:
                 f.write(f"Do some reporting on the Problem, {prob._name}\n")
 
-        register_report(user_defined_report, 'user defined report', 'setup', 'pre', 'problem')
+        register_report(user_defined_report, 'user defined report', 'setup', 'pre')
 
         prob = om.Problem()
         model = prob.model
@@ -199,8 +185,7 @@ class TestReportGeneration(unittest.TestCase):
         prob.run_driver()
         prob.cleanup()
 
-        reports_dir = '.'
-        problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{prob._name}_reports')
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{prob._name}_reports')
 
         p = pathlib.Path(problem_reports_dir).joinpath(user_report_filename)
         self.assertTrue(p.is_file(),f'The user defined report file, {str(p)} was not found')
@@ -208,7 +193,6 @@ class TestReportGeneration(unittest.TestCase):
     def test_report_generation_various_locations(self):
         # the reports can be generated pre and post for setup, final_setup, and run_driver
         # check those all work
-        import os
         from openmdao.utils.reports import register_report
 
         def user_defined_report(prob, filename):
@@ -219,7 +203,7 @@ class TestReportGeneration(unittest.TestCase):
             for pre_or_post in ['pre', 'post']:
                 user_report_filename = f'{pre_or_post}_{method}.txt'
                 register_report(user_defined_report, f'user defined report {pre_or_post} {method}', method,
-                                pre_or_post, 'problem', filename=user_report_filename)
+                                pre_or_post, filename=user_report_filename)
 
         prob = om.Problem()
         model = prob.model
@@ -238,8 +222,7 @@ class TestReportGeneration(unittest.TestCase):
         prob.run_driver()
         prob.cleanup()
 
-        reports_dir = '.'
-        problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{prob._name}_reports')
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{prob._name}_reports')
 
         for method in ['setup', 'final_setup', 'run_driver']:
             for pre_or_post in ['pre', 'post']:
@@ -257,7 +240,6 @@ class TestReportGeneration(unittest.TestCase):
                 self._problem = None
 
             def solve(self):
-                # create a simple subproblem and run it to test for global solver_info bug
                 p = om.Problem(name=self.prob_name)
                 self._problem = p
                 p.model.add_subsystem('indep', om.IndepVarComp('x', 1.0))
@@ -266,17 +248,8 @@ class TestReportGeneration(unittest.TestCase):
                 p.setup()
                 p.run_model()
 
-                print(p.msginfo)
-
                 return super().solve()
 
-        # TODO just run all of them? To show how you can pass different of and wrt to coloring report
-        # to simplify things, just do n2. There are issues with coloring and scaling reports
-        # reports_registry = []
-        # from openmdao.visualization.n2_viewer.n2_viewer import n2
-        # from openmdao.utils.reports import register_report
-        # register_report(n2, 'create n2', 'final_setup', 'post', 'problem', probname=None,
-        #                 show_browser=False)
 
         # Initially use the default names
         openmdao.core.problem._problem_names = []  # need to reset these to simulate separate runs
@@ -289,13 +262,13 @@ class TestReportGeneration(unittest.TestCase):
         p.setup()
         p.run_model()  # need to do run_model in this test so sub problem is created
 
-        reports_dir = '.'
+        # reports_dir = '.'
 
         # Check existence of files for problem1
         problem1_name = p._name
         problem2_name = G.nonlinear_solver._problem._name
         for problem_name in [problem1_name, problem2_name]:
-            problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{problem_name}_reports')
+            problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{problem_name}_reports')
             p = pathlib.Path(problem_reports_dir).joinpath(self.n2_filename)
             self.assertTrue(p.is_file(),f'The N2 report file, {str(p)} was not found')
             p = pathlib.Path(problem_reports_dir).joinpath(self.scaling_filename)
@@ -304,7 +277,8 @@ class TestReportGeneration(unittest.TestCase):
             self.assertFalse(p.is_file(),f'The coloring report file, {str(p)}, was found but should not have')
 
     def test_report_generation_multiple_problems_report_specific_problem(self):
-        import openmdao.core.problem
+        # test the ability to register a report with a specific Problem name rather
+        #   than have the report run for all Problems
         class _ProblemSolver(om.NonlinearRunOnce):
 
             def __init__(self, prob_name=None):
@@ -313,7 +287,6 @@ class TestReportGeneration(unittest.TestCase):
                 self._problem = None
 
             def solve(self):
-                # create a simple subproblem and run it to test for global solver_info bug
                 p = om.Problem(name=self.prob_name)
                 self._problem = p
                 p.model.add_subsystem('indep', om.IndepVarComp('x', 1.0))
@@ -322,26 +295,14 @@ class TestReportGeneration(unittest.TestCase):
                 p.setup()
                 p.run_model()
 
-                print(p.msginfo)
-
                 return super().solve()
 
-        # TODO
-
-        # TODO just run all of them? To show how you can pass different of and wrt to coloring report
         # to simplify things, just do n2. There are issues with coloring and scaling reports
-        from openmdao.utils.reports import clear_reports, list_reports
         clear_reports()
-        list_reports()
-        from openmdao.visualization.n2_viewer.n2_viewer import n2
-        from openmdao.utils.reports import register_report
-        register_report(n2, 'create n2', 'final_setup', 'post', 'problem', probname='problem2',
+        register_report(n2, 'create n2', 'final_setup', 'post', probname='problem2',
                         show_browser=False)
 
-        list_reports()
-
         # Initially use the default names
-        openmdao.core.problem._problem_names = []  # need to reset these to simulate separate runs
         p = om.Problem()
         p.model.add_subsystem('indep', om.IndepVarComp('x', 1.0))
         G = p.model.add_subsystem('G', om.Group())
@@ -351,13 +312,46 @@ class TestReportGeneration(unittest.TestCase):
         p.setup()
         p.run_model()  # need to do run_model in this test so sub problem is created
 
-        reports_dir = '.'
-        probname = G.nonlinear_solver._problem._name
-        problem_reports_dir = pathlib.Path(reports_dir).joinpath(f'{probname}_reports')
+        probname = p._name
+        subprobname = G.nonlinear_solver._problem._name
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{subprobname}_reports')
 
         p = pathlib.Path(problem_reports_dir).joinpath('n2.html')
         self.assertTrue(p.is_file(),f'The user defined report file, {str(p)} was not found')
 
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{probname}_reports')
+        p = pathlib.Path(problem_reports_dir).joinpath('n2.html')
+        self.assertFalse(p.is_file(), f'The user defined report file, {str(p)} was found but should not have')
+
+    def test_report_generation_test_TESTFLO_RUNNING(self):
+        os.environ['TESTFLO_RUNNING'] = 'true'
+
+        prob = om.Problem()
+        model = prob.model
+
+        model.add_subsystem('p1', om.IndepVarComp('x', 0.0), promotes=['x'])
+        model.add_subsystem('p2', om.IndepVarComp('y', 0.0), promotes=['y'])
+        model.add_subsystem('comp', Paraboloid(), promotes=['x', 'y', 'f_xy'])
+
+        model.add_design_var('x', lower=0.0, upper=1.0)
+        model.add_design_var('y', lower=0.0, upper=1.0)
+        model.add_objective('f_xy')
+
+        prob.driver = om.ScipyOptimizeDriver()
+
+        prob.setup()
+        prob.run_driver()
+        prob.cleanup()
+
+        # get the path to the problem subdirectory
+        problem_reports_dir = pathlib.Path(_reports_dir).joinpath(f'{prob._name}_reports')
+
+        p = pathlib.Path(problem_reports_dir).joinpath(self.n2_filename)
+        self.assertFalse(p.is_file(),f'The N2 report file, {str(p)} was found but should not have')
+        p = pathlib.Path(problem_reports_dir).joinpath(self.scaling_filename)
+        self.assertFalse(p.is_file(),f'The scaling report file, {str(p)}, was found but should not have')
+        p = pathlib.Path(problem_reports_dir).joinpath(self.coloring_filename)
+        self.assertFalse(p.is_file(),f'The coloring report file, {str(p)}, was not found')
 
 
 
